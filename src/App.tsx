792a1005216f84783905f4e0582a08106c81b08d
Kaio Siqueira <ikaiodev@gmail.com>
2026-09-07 21:39:40 -0300

    feat(toil) introduced

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,60 @@
 import ListDeliveryReminders, {
-  CreateDeliveryReminderButton,
-  useDeliveryReminders,
-} from "./DeliveryReminders";
+	CreateDeliveryReminderButton,
+	useDeliveryReminders,
+} from "./Reminders";
+
 import { Title } from "./StaticComponents";
 
+function ReminderSection() {
+	const { reminders, addReminder, removeReminder } = useDeliveryReminders();
+
+	return (
+		<div className="p-4 border border-black/20 shadow-lg rounded">
+			<div className="flex justify-between gap-2">
+				<div>
+					<Title>Lembrete de eventos</Title>
+					<p className="text-xs">
+						Gerencie lembretes para não esquecer de receber dinheiro de
+						entregadores.
+					</p>
+				</div>
+
+				<CreateDeliveryReminderButton onSave={addReminder} />
+			</div>
+
+			<ListDeliveryReminders reminders={reminders} onRemove={removeReminder} />
+		</div>
+	);
+}
+
+function ToilSection() {
+	const { reminders, addReminder, removeReminder } = useDeliveryReminders();
+
+	return (
+		<div className="p-4 border border-black/20 shadow-lg rounded">
+			<div className="flex justify-between gap-2">
+				<div>
+					<Title>Banco de horas</Title>
+					<p className="text-xs">
+						Gerenciamento de horas extras, feriados e faltas.
+					</p>
+				</div>
+
+				<CreateDeliveryReminderButton onSave={addReminder} />
+			</div>
+
+			<ListDeliveryReminders reminders={reminders} onRemove={removeReminder} />
+		</div>
+	);
+}
+
 function App() {
-  const { reminders, addReminder, removeReminder } = useDeliveryReminders();
-
-  return (
-    <main className="px-10 py-5">
-      <div className="p-4 border-2 border-black/10 shadow-md rounded">
-        <div className="flex justify-between gap-2">
-          <div>
-            <Title>Lembretes pendentes</Title>
-            <p className="text-xs">
-              eg. Dinheiro a receber de entregadores ou pagamentos pendentes para ainda hoje
-            </p>
-          </div>
-
-          <CreateDeliveryReminderButton onSave={addReminder} />
-        </div>
-
-        <ListDeliveryReminders
-          reminders={reminders}
-          onRemove={removeReminder}
-        />
-      </div>
-    </main>
-  );
+	return (
+		<main className="flex flex-col gap-4 px-10 py-5">
+			<ReminderSection />
+			<ToilSection />
+		</main>
+	);
 }
 
 export default App;
